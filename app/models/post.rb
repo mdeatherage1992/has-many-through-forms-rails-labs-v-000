4f80d368f046632dcfcebbd372bbf28e55939248
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-13 12:32:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(categories_hashes) 
-    categories_hashes.each do |i, category_attributes| 
+    categories_hashes.each do |i, category_attributes|
       if category_attributes[:name].present?
         category = Category.find_or_create_by(name: category_attributes[:name])
         if !self.categories.include?(category)
